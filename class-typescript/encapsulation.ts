e2810de44c0542b38818642b47553ac77a94575d
Md Shakil Ahmed <shakilatrai5@gmail.com>
2023-05-30 07:52:20 +0600

    tsconfig added

--- class-typescript/encapsulation.ts
+++ class-typescript/encapsulation.ts
@@ -2,11 +2,12 @@ class User {
     //Properties, method, constructor
     public userName: string;
     private age: number;
-
+   
     constructor(userName: string, age: number) {
         this.userName = userName;
         this.age = age;
     }
+    
 
     display(): void {
         console.log(`username: ${this.userName}, age: ${this.age} `);
